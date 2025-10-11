8b1c94acd74fad013b14be951a1f9b2e1f3f9b50
nqbao1002 <nqbao113@gmail.com>
2025-10-11 20:31:13 +0700

    xong upload ảnh cho chat

--- src/main/java/capstone_project/service/services/room/ChatService.java
+++ src/main/java/capstone_project/service/services/room/ChatService.java
@@ -1,9 +1,12 @@
 package capstone_project.service.services.room;
 
+import capstone_project.dtos.request.room.ChatImageRequest;
 import capstone_project.dtos.request.room.MessageRequest;
 import capstone_project.dtos.response.room.ChatPageResponse;
 import capstone_project.dtos.response.room.ChatResponseDTO;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
@@ -15,4 +18,6 @@ public interface ChatService {
             throws ExecutionException, InterruptedException;
 
     ChatPageResponse getMessagesForRoomSupportForCusByUserId(UUID userId, int pageSize, String lastMessageId) throws ExecutionException, InterruptedException;
+
+    String uploadChatImage(ChatImageRequest chatImageRequest) throws IOException;
 }
